3bb43b14206a07cb166482fb94098dbc5a1efd74
janaylson-369 <janailson.gomes@academico.ifpb.edu.br>
2025-08-26 13:49:07 -0300

    Update preparacoes.js
    add de comentarios

--- SRC/dataset/preparacoes.js
+++ SRC/dataset/preparacoes.js
@@ -1,14 +1,14 @@
+// array inicial com algumas fruteiras pré-cadastradas, cada objeto representa uma fruteira(card) com suas propriedades básicas
+
 let fruteirasCard = [
     {
-        
-        src: "/assets/image/manga.png",
-        nomeEspecie: "Manga",
-        nomeCientifico: "Mangifera indica",
-        producaoMedia: "150",
-        dataPlantio: "2020-01-15"
+        src: "/assets/image/manga.png", // Caminho da imagem da fruta
+        nomeEspecie: "Manga",           // Nome popular
+        nomeCientifico: "Mangifera indica", // Nome científico
+        producaoMedia: "150",          // Produção média em Kg
+        dataPlantio: "2020-01-15"      // Data de plantio no formato ISO
     },
     {
-        
         src: "/assets/image/acerola.png",
         nomeEspecie: "Acerola",
         nomeCientifico: "Malpighia emarginata",
@@ -16,7 +16,6 @@ let fruteirasCard = [
         dataPlantio: "2021-06-10"
     },
     {
-        
         src: "/assets/image/caju.png",
         nomeEspecie: "Caju",
         nomeCientifico: "Anacardium occidentale",
@@ -24,7 +23,6 @@ let fruteirasCard = [
         dataPlantio: "2019-09-20"
     },
     {
-        
         src: "/assets/image/abacate.png",
         nomeEspecie: "Abacate",
         nomeCientifico: "Persea americana",
@@ -33,9 +31,9 @@ let fruteirasCard = [
     }
 ];
 
-
-let preparacoesfrutas = [];
+let preparacoesfrutas = [];// uma array vazia
 
 let inserirfrutas = () => { };
-// Exporta as variáveis para que possam ser usadas em outros arquivos .
-export { fruteirasCard, preparacoesfrutas, inserirfrutas };
+
+export { fruteirasCard, preparacoesfrutas, inserirfrutas };// exporta as variáveis e funções para que possam ser usadas em outros arquivos, isso irá permitir a modularizaçao do código
+
